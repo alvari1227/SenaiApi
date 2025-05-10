ef950413f460c693872e98212ad684660abbb7d7
JOÃO ALVARI RODRIGUES DA SILVA <joao-alvari_silva@estudante.sesisenai.org.br>
2025-05-10 11:58:21 -0300

    Primeiro commit

---
+++ Contexto/SenaiContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entidades;
+
+namespace WebApplication1.Contexto
+{
+    public class SenaiContext : DbContext
+    {
+        public DbSet<Escola> Escola { get; set; }
+
+        public DbSet<Escola> Endereço { get; set; }
+
+        public DbSet<Escola> Professor { get; set; }
+
+        public DbSet<Escola> Classe { get; set; }
+
+        public DbSet<Escola> Aluno { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=Senai;User Id=postgres;Password=root;");
+        }
+
+
+
+    }
+}
---
+++ Migrations/20250510142534_Meu commit inicial.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Meucommitinicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Escola",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Escola", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereço",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Rua = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    Bairro = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Cidade = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Estado = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    Numero = table.Column<int>(type: "integer", nullable: false),
+                    EscolaId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereço", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereço_Escola_EscolaId",
+                        column: x => x.EscolaId,
+                        principalTable: "Escola",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    EscolaId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Professor_Escola_EscolaId",
+                        column: x => x.EscolaId,
+                        principalTable: "Escola",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classe",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Serie = table.Column<int>(type: "integer", nullable: false),
+                    EscolaId = table.Column<long>(type: "bigint", nullable: false),
+                    ProfessorId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Classe_Escola_EscolaId",
+                        column: x => x.EscolaId,
+                        principalTable: "Escola",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Classe_Professor_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classe_EscolaId",
+                table: "Classe",
+                column: "EscolaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classe_ProfessorId",
+                table: "Classe",
+                column: "ProfessorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereço_EscolaId",
+                table: "Endereço",
+                column: "EscolaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Professor_EscolaId",
+                table: "Professor",
+                column: "EscolaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classe");
+
+            migrationBuilder.DropTable(
+                name: "Endereço");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+
+            migrationBuilder.DropTable(
+                name: "Escola");
+        }
+    }
+}
